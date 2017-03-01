99a65bdc933815ccdde2c043fdbcf95cee638795
Serene Careaga <info@serenecareaga.com>
2017-03-01 11:53:22 -0500

    logger showing successful connection

---
+++ lib/boom.ex
@@ -0,0 +1,48 @@
+defmodule Boom do
+  require Logger
+  use Application
+  @interface :wlan0
+  @kernel_modules Mix.Project.config[:kernel_modules] || []
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # worker(Boom.Worker, [arg1, arg2, arg3]),
+      worker(Task, [fn -> blinker() end], id: Boom.Blinker),
+      worker(Task, [fn -> init_kernel_modules() end], restart: :transient, id: Nerves.Init.KernelModules),
+      worker(Task, [fn -> init_wifi_network() end], restart: :transient, id: Nerves.Init.WifiNetwork),
+      worker(Task, [fn -> talker() end], id: Boom.Talker)
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Boom.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def blinker() do
+    Nerves.Leds.set green: true
+    :timer.sleep 200
+    Nerves.Leds.set green: false
+    :timer.sleep 200
+    blinker()
+  end
+
+  def init_kernel_modules() do
+    Enum.each(@kernel_modules, & System.cmd("modprobe", [&1]))
+  end
+
+  def init_wifi_network() do
+    Nerves.InterimWiFi.setup @interface, Application.get_env(:boom, @interface)
+  end
+  
+  def talker() do
+    Logger.info "Hello from Serene :D"
+    :timer.sleep 1000
+    talker()
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,48 @@
+defmodule Boom.Mixfile do
+  use Mix.Project
+  @target System.get_env("NERVES_TARGET") || "rpi3"
+
+  def project do
+    [app: :boom,
+     version: "0.1.0",
+     target: @target,
+     archives: [nerves_bootstrap: "~> 0.2.1"],
+     
+     deps_path: "deps/#{@target}",
+     build_path: "_build/#{@target}",
+     
+     kernel_modules: kernel_modules(@target),
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases(),
+     deps: deps() ++ system(@target)]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [mod: {Boom, []},
+     extra_applications: [:logger, :nerves_leds]]
+  end
+
+  def deps do
+    [{:nerves, "~> 0.4.0"},
+     {:nerves_leds, "~> 0.8.0"},
+     {:nerves_interim_wifi, "~> 0.1"},
+     {:logger_multicast_backend, "~> 0.2"}]
+  end
+
+  def system(target) do
+    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  end
+
+  def aliases do
+    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  end
+ 
+  def kernel_modules("rpi3"), do: ["brcmfmac"]
+  def kernel_modules(_), do: []
+
+end
